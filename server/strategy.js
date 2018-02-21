681a6f0f6fe03b0753deae26e215e34456cfcc6d
Michael Koerber <michaelkoerber@Michaels-MacBook-Pro.local>
2018-02-21 15:25:17 -0600

    Finished

---
+++ server/strategy.js
@@ -0,0 +1,12 @@
+const Auth0Strategy = require('passport-auth0');
+const { domain, clientID, clientSecret } = require(`${__dirname}/../config`);
+
+module.exports = new Auth0Strategy({
+    domain: domain,
+    clientID: clientID,
+    clientSecret: clientSecret,
+    scope: "profile openid",
+    callbackURL: '/login'
+}, function(accessToken, refreshToken, extraParams, profile, done) {
+    return done(null, profile);
+});
